87cb5a0bbc075b1a2590e5d2b79b8590af7b1ac7
ammann64 <austinmmann64@gmail.com>
2023-11-13 12:09:16 -0600

    Add login, sign up, and logout routes and forms

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,30 @@
 const router = require('express').Router();
-
+const { Comment, Post, User} = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
     try {
-        
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name']
+                },
+                {
+                    model: Comment,
+                    attributes: ['content', 'dateCreated']
+                }
+            ]
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('homepage', {
+            posts,
+            logged_in: req.session.logged_in
+        });
+
     } catch (error) {
-        
+        res.status(500).json(err);
     }
 });
